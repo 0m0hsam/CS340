98abab43aebe9e80d0783a69e77d585d53cb3f29
0m0hsam <omohsam81@gmail.com>
2025-07-05 04:50:49 +0100

    starting

--- server.js
+++ server.js
@@ -4,11 +4,17 @@
  *******************************************/
 /* ***********************
  * Require Statements
+
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+app.set("view engine","ejs")
+app.use(expressLayouts)
+app.set("layout","./layouts/layout")
 
 /* ***********************
  * Routes
